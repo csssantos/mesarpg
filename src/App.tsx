846c40fa79f89a712fc15474b6d33bbb639e7909
csssantos <cssssantos75@gmail.com>
2026-01-07 03:10:02 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // src/App.tsx
+import { useEffect } from 'react'; // <--- ADICIONE ESTA IMPORTAÇÃO AQUI
 import { useGameStore } from './store/useGameStore';
 import { MapBoard } from './components/MapBoard';
 import { Sidebar } from './components/Sidebar';
@@ -8,11 +9,11 @@ import { CharacterSelectionPage } from './components/CharacterSelectionPage';
 import "./App.css";
 
 function App() {
-const isPlotTwistActive = useGameStore((state) => state.isPlotTwistActive);
+  const isPlotTwistActive = useGameStore((state) => state.isPlotTwistActive);
   const activeSessionPlayerId = useGameStore((state) => state.activeSessionPlayerId);
-  const connectToDB = useGameStore((state) => state.connectToDB); // <--- Pegue a ação
+  const connectToDB = useGameStore((state) => state.connectToDB);
 
-  // --- CONECTAR AO FIREBASE AO ABRIR ---
+  // Conectar ao Banco de Dados ao iniciar
   useEffect(() => {
     connectToDB();
   }, []);
@@ -21,7 +22,6 @@ const isPlotTwistActive = useGameStore((state) => state.isPlotTwistActive);
     return <CharacterSelectionPage />;
   }
 
-  // Se JÁ tem jogador selecionado, mostra o jogo principal
   return (
     <div className={`bg-neutral-950 min-h-screen text-white font-sans transition-all duration-[3000ms] overflow-hidden ${isPlotTwistActive ? 'grayscale-0' : 'bg-slate-950'}`}>
       <Sidebar />
@@ -29,10 +29,8 @@ const isPlotTwistActive = useGameStore((state) => state.isPlotTwistActive);
       <GameMasterPanel />
       
       <main className="pl-24 pr-4 py-4 h-screen relative">
-        {/* Camada de Névoa de Wychwood (Reutilizando a classe do seu CSS) */}
         {!isPlotTwistActive && <div className="fog-overlay" />}
 
-        {/* Header Narrativo */}
         <div className="absolute top-6 left-32 z-40 max-w-md pointer-events-none select-none">
           <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600 filter drop-shadow-lg">
             {isPlotTwistActive ? "Rodovia BR-101" : "Mistérios de Wychwood"}
